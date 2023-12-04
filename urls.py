aad4f6452a00365c0fdf94e518d7d18956d57c3d
linnujose <94525822+linnujose@users.noreply.github.com>
2023-12-04 23:40:24 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -4,6 +4,19 @@ from . import views
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static  import static
+from .views import remove_from_cart  # Import the remove_from_cart view
+from .views import view_cart  # Import the view_cart function
+from .views import increase_cart_item  # Import the increase_cart_item function
+from .views import decrease_cart_item  # Import the decrease_cart_item function
+from .views import fetch_cart_count
+
+
+
+
+
+
+
+
 
 urlpatterns = [
    path('',views.index,name="index"),
@@ -17,20 +30,61 @@ urlpatterns = [
    path('add_product/',views.add_product,name="add_product"),
    path('view_product/',views.view_product,name="view_product"),
    path('product_list/',views.product_list,name="product_list"),
+   path('product_list/<slug:category_slug>/', views.product_list, name='product_list_category'),
+    path('product_list/<slug:category_slug>/<slug:subcategory_slug>/', views.product_list, name='product_list_subcategory'),
+
+   #  path('increase_cart_item/<int:product_id>/', views.increase_cart_item, name='increase_cart_item'),
+   #  path('decrease_cart_item/<int:product_id>/', views.decrease_cart_item, name='decrease_cart_item'),
+
+   #  path('add-to-cart/<int:product_id>/', add_to_cart, name='add-to-cart'),
+   #  path('remove-from-cart/<int:product_id>/', remove_from_cart, name='remove-from-cart'),
+    
+
+
+
    path('edit_product/<int:product_id>/', views.edit_product, name='edit_product'),
    path('deactivate_product/<int:product_id>/', views.deactivate_product, name='deactivate_product'),
    
    #linnus add to whislist and cart
    path('product/<int:product_id>/', views.product_details, name='product_details'),
-   path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+   path('add_to_cart/<int:id>/', views.add_to_cart, name='add_to_cart'),
    path('add_to_wishlist/<int:product_id>/', views.add_to_wishlist, name='add_to_wishlist'),
     
    path('cart/', views.cart, name='cart'),
    path('wishlist/', views.wishlist, name='wishlist'),
    path('remove_from_wishlist/<int:product_id>/', views.remove_from_wishlist, name='remove_from_wishlist'),
+   path('userprofile/', views.userprofile, name='userprofile'),
+
+   path('changepassword/', views.changepassword, name='changepassword'),
+   path('shop/', views.shop, name='shop'),
+   path('base/', views.base, name='base'),
+   path('navbar/', views.navbar, name='navbar'),
+   path('account/', views.account, name='account'),
 
 
-   
+   path('remove-from-cart/<int:product_id>/',views.remove_from_cart, name='remove-from-cart'),
+   # path('cart/', view_cart, name='cart'),
+   path('increase-cart-item/<int:product_id>/',views.increase_cart_item, name='increase-cart-item'),
+    path('decrease-cart-item/<int:product_id>/',views.decrease_cart_item, name='decrease-cart-item'),
+   path('fetch-cart-count/',views.fetch_cart_count, name='fetch-cart-count'),
+
+
+   #userprofile
+   path('save_profile/', views.save_profile, name='save_profile'),
+
+
+   #payment
+   # path('checkout/', views.checkout, name='checkout'),
+
+   path('fetch-cart-count/',views.fetch_cart_count, name='fetch-cart-count'),
+   path('create-order/', views.create_order, name='create-order'),
+   path('handle-payment/',views.handle_payment, name='handle-payment'),
+   path('checkout/',views.checkout, name='checkout'),
+
+
+   #billinvoice
+   # path('billinvoice/', views.bill_invoice, name='bill_invoice'),
+   path('billinvoice/', views.bill_invoice, name='bill_invoice'),
  
       # forgot password
 
